f9ebe1de3bb13cb52c7ba95ee47c043f59b150e0
Tero Tolonen <digikansio@gmail.com>
2015-06-17 22:12:27 +0300

    "commit message"

--- release/socketEmulator.js
+++ release/socketEmulator.js
@@ -1417,24 +1417,26 @@ io.on('connection', function(socket){
             }]);
           })
           me._db = event.target.result;
-          later().add(
-            function() {
-              if (options && options.tables) {
-                for (var n in options.tables) {
-                  if (options.tables.hasOwnProperty(n)) {
-                    var opts = options.tables[n];
-                    // Create another object store called "names" with the autoIncrement flag set as true.    
-                    try {
-                      var objStore = me._db.createObjectStore(n, opts.createOptions);
-                    } catch (e) {
-
+          /*
+  later().add( 
+      function() {
+            if(options && options.tables) {
+                for(var n in options.tables) {
+                    if(options.tables.hasOwnProperty(n)) {
+                        var opts = options.tables[n];
+                        // Create another object store called "names" with the autoIncrement flag set as true.    
+                        try {
+                            var objStore = me._db.createObjectStore(n, opts.createOptions);
+                        } catch(e) {
+                            
+                        }
                     }
-                  }
                 }
-              }
-              me.resolve(true);
-            });
-
+            }  
+            me.resolve(true);
+      });
+  */
+          me.resolve(true);
 
         };
         request.onupgradeneeded = function(event) {
